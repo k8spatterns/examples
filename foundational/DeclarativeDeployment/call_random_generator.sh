2ee9928f244cac2a88b4fcaedf20a43c8db6a18c
Roland Huß <rhuss@redhat.com>
2023-03-21 09:38:03 +0100

    DeclarativeDeployment: working on

--- foundational/DeclarativeDeployment/call_random_generator.sh
+++ foundational/DeclarativeDeployment/call_random_generator.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+# When using a Service with NodePort, you can use this script to find out the port and address
+# and then perodically access this service:
+
 # Pick up service's NodePort
 port=$(kubectl get svc random-generator -o jsonpath={.spec.ports[0].nodePort})
 # Take from the first's node the hostname which should be reachable from here
