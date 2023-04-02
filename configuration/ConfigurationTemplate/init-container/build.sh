514081c2cb3b25ec5643237aaa3459955c3270a5
Roland Huß <rhuss@redhat.com>
2023-04-02 16:38:55 +0200

    Worked on configuration templates

--- configuration/ConfigurationTemplate/init-container/build.sh
+++ configuration/ConfigurationTemplate/init-container/build.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 repo=k8spatterns/example-configuration-template-init
 echo "Building ---> ${repo}"
-docker build --no-cache -t ${repo} .
+extra_args=""
 if [ "x$1" == "x-p" ]; then
-  docker push ${repo}
+  extra_args="--push"
 fi
+docker buildx create --use
+docker buildx build --platform linux/arm64/v8,linux/amd64 $extra_args -t $repo .
