aba5ae7cc48c5af9925a791001d8803cb8369c73
Roland Huß <rhuss@redhat.com>
2023-04-03 11:26:14 +0200

    Fixed expose example

--- advanced/Controller/expose-controller/expose-controller.sh
+++ advanced/Controller/expose-controller/expose-controller.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 
-# Call it with the namespace to monitor
-namespace=${1:-default}
+# Namespace to watch (or 'default' if not given)
+namespace=${WATCH_NAMESPACE:-default}
 
 # API URL setup. Requires an ambassador API proxy running side-by-side
 base=http://localhost:8001
 ns=namespaces/$namespace
 k8s_service_url=$base/api/v1/$ns/services
-k8s_ingress_url=$base/apis/extensions/v1beta1/$ns/ingresses
+k8s_ingress_url=$base/apis/networking.k8s.io/v1/$ns/ingresses
 
 # Watch the K8s API on events on service objects
 echo "::: Starting to wait for events"
-curl -N -s -k $k8s_service_url?watch=true | while read event
+curl -N -s $k8s_service_url?watch=true | while read -r event
 do
   # Sanitize new lines
-  event=$(echo $event | tr '\r\n' ' ')
-
+  event=$(echo "$event" | tr '\r\n' ' ')
   # Event type
   type=$(echo $event | jq -r '.type')
   # Annotation "expose" on service
